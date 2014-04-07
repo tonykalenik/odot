2489cc15d9879caefd9029983e531f65eac9d201
Anton Kalenik <anton@kalenik.name>
2014-04-07 20:16:23 +0300

    Еще один тест

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,5 @@
 class TodoList < ActiveRecord::Base
+  has_many :todo_items
   validates :title, presence: true, length: {minimum: 3}
   validates :description, presence: true, length: {minimum: 5}
 end
